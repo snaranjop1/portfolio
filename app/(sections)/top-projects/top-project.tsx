d8f9dfc523ce110bf3f49df647ffa542e03c8603
Sergio Naranjo Puentes <sergionaranjopuentes@gmail.com>
2025-02-27 18:00:43 -0500

    fix: enhance top troject card styles

--- app/(sections)/top-projects/top-project.tsx
+++ app/(sections)/top-projects/top-project.tsx
@@ -13,20 +13,20 @@ interface TopProjectProps {
 export function TopProject({ name, description, url, github, type }: TopProjectProps) {
   const t = useTranslations('topProjects');
   return (
-    <div className='border border-muted shadow-sm rounded-md bg-muted p-4 flex flex-col gap-4 lg:items-center lg:flex-row lg:justify-between'>
-      <div className='space-y-2'>
-        <a
-          target='_blank'
-          href={url ?? github ?? '#'}
-          className='font-semibold items-center hover:cursor-pointer underline hover:text-primary'
-        >
-          {name}
-        </a>
-        <p className='text-sm font-light'>{description}</p>
-        <Badge className='text-xs font-light mt-1'>
-          {type === 'personal' ? t('personal') : t('job')}
-        </Badge>
+    <a
+      target='_blank'
+      href={url ?? github ?? '#'}
+      className='block transition-transform hover:scale-[1.02] duration-100 group'
+    >
+      <div className='border border-muted shadow-sm rounded-md bg-muted p-4 flex flex-col gap-4 lg:items-center lg:flex-row lg:justify-between'>
+        <div className='space-y-2'>
+          <span className='font-semibold items-center group-hover:text-primary'>{name}</span>
+          <p className='text-sm font-light'>{description}</p>
+          <Badge className='text-xs font-light mt-1'>
+            {type === 'personal' ? t('personal') : t('job')}
+          </Badge>
+        </div>
       </div>
-    </div>
+    </a>
   );
 }
